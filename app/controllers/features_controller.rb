1f6f65ebf6ad3a9834c14f49b4441877a241e681
Ryan <yuwei.ryan@snda.com>
2014-06-17 15:09:44 +0800

    add new riskmanagement files to the repository

---
+++ app/controllers/features_controller.rb
@@ -0,0 +1,24 @@
+class FeaturesController < ApplicationController
+  def index
+    @feature = "this is a test feature"
+  end
+  
+  def create
+  end
+  
+  def new
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+end
